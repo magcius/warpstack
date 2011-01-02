78937f11a807a791d90ffbac5514d59fe4021129
Jasper St. Pierre <jstpierre@mecheye.net>
2011-01-02 14:08:49 -0500

    Version bump.

--- WarpStack.java
+++ WarpStack.java
@@ -3,7 +3,7 @@ import java.util.LinkedHashMap;
 
 public class WarpStack extends Plugin {
     public String name = "WarpStack";
-    public String version = "0.9";
+    public String version = "0.10";
 
     public static int stack_limit;
     public static boolean override;
